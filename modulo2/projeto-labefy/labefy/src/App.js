d8b63966d6788abdd99fd05a3caa130b06f8379c
servuloccsilva <servuloccsilva@gmail.com>
2022-04-07 16:40:39 -0300

    Início do Labefy

---
+++ modulo2/projeto-labefy/labefy/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+     Labefy - O melhor lugar pra você ouvir suas músicas
+    </div>
+  );
+}
+
+export default App;
