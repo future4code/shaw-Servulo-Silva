a55141152bd88715076b1cd5b180139877086acc
servuloccsilva <servuloccsilva@gmail.com>
2022-04-22 18:11:59 -0300

    Astromatch - quase finalizado

--- modulo3/astromatch/astromatch-app/src/App.js
+++ modulo3/astromatch/astromatch-app/src/App.js
@@ -1,11 +1,42 @@
-import React from "react";
+import React, {useState} from "react";
+import styled from "styled-components";
+import axios from "axios";
+import TelaInicial from "./components/TelaInicial/TelaInicial";
+import TelaMatches from "./components/TelaMatches/TelaMatches";
 
 function App() {
+  const [paginaAtual, setPaginaAtual] = useState("inicial")
+
+  const irParaInicial = () => {
+    setPaginaAtual("inicial")
+  }
+  const irParaMatches = () => {
+    setPaginaAtual("matches")
+  }
+
+  const escolherTela = () => {
+    switch (paginaAtual) {
+      case "inicial":
+        return <TelaInicial
+          irParaInicial = {irParaInicial}
+          irParaMatches = {irParaMatches}
+          />
+        
+      case "matches":
+        return <TelaMatches 
+          irParaInicial = {irParaInicial}
+          irParaMatches = {irParaMatches}
+        />;
+      default:
+        return <TelaInicial />;
+    }
+  }
+  
   return (
     <div>
-     Oi
+     {escolherTela()}
     </div>
-  );
+  )
 }
 
 export default App;
